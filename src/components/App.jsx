aa45addd5b35c23c2fa9b6a33c650c568eef9aeb
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-15 23:35:29 +0000

    separated the time states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 
 function App() {
-
   setInterval(getTime, 1000);
 
   let cTime = new Date().toLocaleTimeString();
 
   const [time, setCount] = useState(cTime);
+
+  // for hours
+  let getHours = new Date().getHours();
+  console.log(getHours);
+  const [gtHours, setHours] = useState(getHours);
+
+  // for munites
+  let getMin = new Date().getMinutes();
+  console.log(getHours);
+  const [gtMin, setMin] = useState(getMin);
+
+  // for seconds
+  let getSec = new Date().getSeconds();
+  console.log(getHours);
+  const [gtSec, setSec] = useState(getSec);
 
   function getTime() {
     let nTime = new Date().toLocaleTimeString();
@@ -17,6 +31,10 @@ function App() {
     <div className="container">
       <h1>{time}</h1>
       <button onClick={getTime}>Get Time</button>
+      <p>Breaking it down</p>
+      <h2>
+        {getHours}hrs : {getMin}min : {getSec}secs
+      </h2>
     </div>
   );
 }
